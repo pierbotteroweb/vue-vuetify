fd95c518ebe6598264524cd30427df95aa7d5c87
Pier Bottero <pierbotteroweb@gmail.com>
2025-05-07 15:14:27 -0300

    App bar, usage of api reference for a specific component

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ Navigation drawers: https://v2.vuetifyjs.com/en/components/navigation-drawers/
 Appplication reference: https://v2.vuetifyjs.com/en/components/application/#default-application-markup
 
 List reference to add a link on v-list-item using prop 'to': https://v2.vuetifyjs.com/en/api/v-list-item/#props
+
+Icons reference: https://pictogrammers.com/libraries/
+
+Styling (Spacing Example): https://v2.vuetifyjs.com/en/styles/spacing/
+
+Bars: https://v2.vuetifyjs.com/en/components/app-bars/
+
+v-bar API: https://v2.vuetifyjs.com/en/api/v-app-bar/#props
--- src/App.vue
+++ src/App.vue
@@ -37,10 +37,49 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <!-- <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar> -->
+    <!-- new app-bar implemented, the "app" directive makes the nav-bar works for the whole app
+    without it, the v-navigation will not be displayed.  -->
+    <!-- shrink-on-scroll this directive would make the app bar to shrink when the page is scrolled -->
+    <!-- For more on that, check the v-bar api link on the README file -->
+
+    <!-- see how the src is pointing to an image with just the name of the file. 
+    This file is available in the "public" folder of this app named as mountains.jpeg -->
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      src="mountains.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Vuetify todo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
